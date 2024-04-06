0fcc0c0832ee78ff7c488804ef5d0448c3d0cdf4
Salman Khan <111654354+sallulearningtocode@users.noreply.github.com>
2024-04-07 01:14:54 +0530

    Update urls.py
    Updated Entry Point of the Website

--- shops/shops/urls.py
+++ shops/shops/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from .views import IndexView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/',include('store.urls')),
+    path('',IndexView.as_view(),name='index'),
 ]
